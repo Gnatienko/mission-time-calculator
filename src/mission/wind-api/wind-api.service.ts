e397afb2ccc718fe706ad5a80f097a24fc6c33f0
Gnatienko <gnatienko@gmail.com>
2024-01-22 13:41:47 +0200

    fix

--- src/mission/wind-api/wind-api.service.ts
+++ src/mission/wind-api/wind-api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common"
 import { HttpService } from "@nestjs/axios"
-import { AxiosResponse } from "axios"
+import { firstValueFrom } from "rxjs"
 
 @Injectable()
 export class WindApiService {
@@ -13,8 +13,7 @@ export class WindApiService {
     const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=wind_direction_10m,wind_speed_10m`
 
     try {
-      const response = await this.httpService.get(apiUrl).toPromise()
-
+      const response = await firstValueFrom(this.httpService.get(apiUrl))
       const windSpeed = response.data.current.wind_speed_10m
       const windDirection = response.data.current.wind_direction_10m
 
